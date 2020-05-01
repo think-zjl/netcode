fd9a8a38a85dcb269395c84c1331e8f24932f498
think-zjl <zhujianliang930106@gmail.com>
2020-05-01 19:05:36 +0800

    Modify udp client for windows

--- udpClient/udpClient.c
+++ udpClient/udpClient.c
@@ -52,17 +52,17 @@ int main(int argc, char* argv[])
 	}
 	SOCKET sclient = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-	sockaddr_in sin;
+	struct sockaddr_in sin;
 	sin.sin_family = AF_INET;
 	sin.sin_port = htons(UDP_SERVER_PORT);
 	sin.sin_addr.S_un.S_addr = inet_addr(UDP_SERVER_IP);
 	int len = sizeof(sin);
 
 	const char* sendData = "Clinet send\n";
-	sendto(sclient, sendData, strlen(sendData), 0, (sockaddr*)& sin, len);
+	sendto(sclient, sendData, strlen(sendData), 0, (struct sockaddr*)&sin, len);
 
 	char recvData[255];
-	int ret = recvfrom(sclient, recvData, 255, 0, (sockaddr*)& sin, &len);
+	int ret = recvfrom(sclient, recvData, 255, 0, (struct sockaddr*)& sin, &len);
 	if (ret > 0){
 		recvData[ret] = 0x00;
 		printf(recvData);
